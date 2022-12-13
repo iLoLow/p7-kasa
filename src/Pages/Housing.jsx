cee9e765f841eb8a8cf5ad1736ab85a83e005b8b
laurent <soyezlaurent@gmail.com>
2022-12-13 23:52:15 +0100

    finalisation gallery.jsx, maj dropdown stars,route

---
+++ src/Pages/Housing.jsx
@@ -0,0 +1,61 @@
+import {useNavigate, useParams} from "react-router-dom";
+import {useEffect, useState} from "react";
+import Profil from "../components/Profil";
+import Stars from "../components/Stars";
+import Gallery from "../components/Gallery";
+import Tags from "../components/Tags";
+import Dropdown from "../components/Dropdown";
+import "../Styles/Housing.css"
+
+
+function Housing() {
+	const {id} = useParams();
+	const [housing, setHousing] = useState({});
+	const navigation = useNavigate();
+
+	useEffect(() => {
+		fetch('../logements.json')
+			.then((response) => response.json())
+			.then((datas) => {
+				const data = datas.find(datahousing => datahousing.id === id);
+				setHousing(data);
+				if (data === undefined) navigation('/error');
+			}).catch(() => navigation("/error"));
+	},[id, navigation]);
+
+	const {title, location, pictures, rating, host, tags, description, equipments} = housing;
+
+	document.title = `Kasa - ${title}`;
+
+	return (
+			<>
+				{pictures &&
+				<Gallery pictures={pictures}/>
+				}
+				<div className="header-housing">
+					<div className="housing-title">
+						<h2>{title}</h2>
+						<p>{location}</p>
+						{tags && < Tags tags={tags}/>}
+					</div>
+					<div className="name">
+						<Profil host={host}/>
+						<Stars rating={rating}/>
+					</div>
+				</div>
+				<div className="housing-body">
+					<Dropdown title="Description">
+						<p>{description}</p>
+					</Dropdown>
+					<Dropdown title="Équipements">
+						{equipments && equipments.map((text, index) => (
+							<p key={index}> {text} </p>
+						))}
+					</Dropdown>
+				</div>
+			</>
+
+	);
+}
+
+export default Housing;
