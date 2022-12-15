8e767dce19288b5ba6c490ac00f6a8a309aac90b
laurent <soyezlaurent@gmail.com>
2022-12-15 11:32:09 +0100

    resolution bug gallery

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -18,31 +18,32 @@ function Gallery({pictures}){
 
     return(
         <section className="gallery">
+            
         {pictures.map((picture, index) =>
             currentSlide === index && (
                 <div key={picture} className="gallery-img">
-                    <span className="gallery-count">
-                        {index +1}/{lengthArray}
-                    </span>
                     <img src={picture} alt="housing"  />
-                    {lengthArray > 1 ?(
-                        <>
-                            <div className="gallery-arrowLeft" onClick={previousSlide}>
-                            <svg fill="#FFFFFF" width="48" height="80" viewBox="0 0 48 80" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M47.04 7.78312L39.92 0.703125L0.359985 40.3031L39.96 79.9031L47.04 72.8231L14.52 40.3031L47.04 7.78312Z" />
-                            </svg>
-                            </div>
-                            <div className="gallery-arrowRight" onClick={nextSlide}>
-                            <svg fill="#FFFFFF" width="48" height="80" viewBox="0 0 48 80"  xmlns="http://www.w3.org/2000/svg">
-                            <path d="M0.960022 72.3458L8.04002 79.4258L47.64 39.8258L8.04002 0.22583L0.960022 7.30583L33.48 39.8258L0.960022 72.3458V72.3458Z"/>
-                            </svg>
-                            </div>
-                        </>
-                    ) : null}
-                </div>
-                
+                </div> 
             )
         )}
+            <span className="gallery-count">
+                {currentSlide +1}/{lengthArray}
+            </span>
+            {lengthArray > 1 ?(
+                <div className="gallery-arrow">
+
+                    <div className="gallery-arrowLeft" onClick={previousSlide}>
+                    <svg fill="#FFFFFF" width="48" height="80" viewBox="0 0 48 80" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M47.04 7.78312L39.92 0.703125L0.359985 40.3031L39.96 79.9031L47.04 72.8231L14.52 40.3031L47.04 7.78312Z" />
+                    </svg>
+                    </div>
+                    <div className="gallery-arrowRight" onClick={nextSlide}>
+                    <svg fill="#FFFFFF" width="48" height="80" viewBox="0 0 48 80"  xmlns="http://www.w3.org/2000/svg">
+                    <path d="M0.960022 72.3458L8.04002 79.4258L47.64 39.8258L8.04002 0.22583L0.960022 7.30583L33.48 39.8258L0.960022 72.3458V72.3458Z"/>
+                    </svg>
+                    </div>
+                </div>
+            ) : null}
         </section>
     )
 
